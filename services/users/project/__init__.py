bb8cd057c5ea4036fe6fc65997076b96cddde079
jess <jess@jessui-MacBookPro.local>
2019-09-16 09:14:51 +0900

    Added setting Docker Config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,8 @@
 
+import os
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+import sys
 
 # instantiate the app
 app = Flask(__name__)
@@ -8,7 +10,9 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+# app.config.from_object('project.config.DevelopmentConfig')  # new
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
 
 
 class UsersPing(Resource):
@@ -19,3 +23,5 @@ class UsersPing(Resource):
         }
 
 api.add_resource(UsersPing, '/users/ping')
+
+# print(app.config, file=sys.stderr)
